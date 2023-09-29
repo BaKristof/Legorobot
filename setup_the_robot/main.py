b05723813953674cba25f1fabc2b61cd761dd1e2
BaKristof <59069543+BaKristof@users.noreply.github.com>
2023-09-29 11:03:06 +0200

    Add files via upload

--- setup_the_robot/main.py
+++ setup_the_robot/main.py
@@ -7,20 +7,49 @@ from pybricks.ev3devices import GyroSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 
+#ev3.speaker.beep()
+def tourn(self, degree):
+    self.robot.turn((degree-self.gs.angle())-15)
+    for x in range(10):
+        self.robot.turn((degree-self.gs.angle())/2)
 
 ev3 = EV3Brick()
+boxsize= 70
 leftm = Motor(Port.B)
 rightm = Motor(Port.C)
 robot = DriveBase(leftm,rightm,55.5,125)
 gs=GyroSensor(Port.S3)
 gs.reset_angle(0)
-robot.turn(80)
-while gs.angle != 90:
-    robot.turn((90-gs.angle())/2)
-
-
 print( str(gs.angle()))
 
-#ev3.speaker.beep()
+robot.straight(boxsize*-3)
+robot.tourn(90)
+robot.straight(boxsize*3)
+wait(5000)
+robot.straight(boxsize*-3)
+robot.tourn(180)
+robot.straight(boxsize*3)
+wait(5000)
+robot.straight(boxsize*-3)
+robot.tourn(270)
+robot.straight(boxsize*3)
+robot.tourn(0)
+robot.straight(boxsize*3)
+wait(5000)
+robot.straight(boxsize*-3)
+robot.tourn(270)
+robot.straight(boxsize*2)
+robot.tourn(180)
+robot.straight(boxsize*3)
+
+
+
+
+
+
+
+
+
+
 
 
